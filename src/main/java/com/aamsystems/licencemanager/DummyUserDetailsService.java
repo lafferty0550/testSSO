150bf3ac42ec6b39632710b696e3e4b4b7d28a16
Илья Машинин <i.mashinin@aamsystems.ru>
2020-10-16 14:34:03 +0300

    Initial commit

---
+++ src/main/java/com/aamsystems/licencemanager/DummyUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.aamsystems.licencemanager;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class DummyUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        System.out.println(username);
+        return new User(username, "notUsed", true, true, true, true,
+                AuthorityUtils.createAuthorityList("ROLE_USER", "ROLE_ADMIN"));
+    }
+}
